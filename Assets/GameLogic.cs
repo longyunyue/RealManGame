83ba312bc6fdc8803b2aa5d04bcbb70205044bef
unknown <longyunyue@mokaitech.com>
2019-03-09 18:01:15 +0800

    add file

---
+++ Assets/GameLogic.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour {
+
+    public enum GameState
+    {
+        None,
+        Start,
+        Pause,
+        End
+    }
+
+    private StageGenerator _stateGenerator;//路径生成器
+
+    /**游戏初始化*/
+    public void GameInit()
+    {
+        _stateGenerator = this.gameObject.GetComponent<StageGenerator>();
+        //需要处理排名记录
+    }
+
+    /**游戏开始*/
+    public void GameReStart()
+    {
+
+    }
+
+    /**游戏暂停*/
+    public void GamePause()
+    {
+        Time.timeScale = 0;
+    }
+
+    /**游戏暂停恢复*/
+    public void GameAwake()
+    {
+        Time.timeScale = 1;
+    }
+
+    /**游戏结束*/
+    public void GameEnd()
+    {
+        //需要处理排名记录
+        Application.Quit();      
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        GameInit();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
